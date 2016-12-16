7b05ffb231006aab1f1ffc9d8e00aca339a9ac82
Kurt Stephens <git@kurtstephens.com>
2016-12-16 09:33:01 -0600

    Indent.

--- lib/control_path/service/application.rb
+++ lib/control_path/service/application.rb
@@ -38,69 +38,69 @@ module ControlPath::Service
 
     PATH_RX = %r{^/(.+?)/?}
 
-      namespace '/api' do
-        get '/?' do
-          endpoints = {
-            '/api/client/PATH' => {
-              methods: [ 'GET' ],
-              description: "Clients GET their PATH; if the content changed since last GET, clients are expected to act.",
-            },
-            '/api/status/PATH'  => {
-              methods: [ 'GET' ],
-              description: "Returns status of client GETs.",
-            },
-            '/api/control/PATH' => {
-              methods: [ 'GET', 'PUT', 'PATCH', 'DELETE' ],
-              description: "GET, PUT, PATCH or DELETE the control for PATH.",
-            },
-          }
-          json_body(server_metadata.merge(endpoints: endpoints))
-        end
+    namespace '/api' do
+      get '/?' do
+        endpoints = {
+          '/api/client/PATH' => {
+            methods: [ 'GET' ],
+            description: "Clients GET their PATH; if the content changed since last GET, clients are expected to act.",
+          },
+          '/api/status/PATH'  => {
+            methods: [ 'GET' ],
+            description: "Returns status of client GETs.",
+          },
+          '/api/control/PATH' => {
+            methods: [ 'GET', 'PUT', 'PATCH', 'DELETE' ],
+            description: "GET, PUT, PATCH or DELETE the control for PATH.",
+          },
+        }
+        json_body(server_metadata.merge(endpoints: endpoints))
+      end
 
-        namespace '/client' do
-          get PATH_RX do
-            control = store.fetch_control!(path)
-          begin
-            store.update_status!(path, control, request, clean_params)
-            rescue => exc
-              logger.error exc
-              control[:status] = 'API-ERROR'
-            end
-            json_body control
+      namespace '/client' do
+        get PATH_RX do
+          control = store.fetch_control!(path)
+        begin
+          store.update_status!(path, control, request, clean_params)
+          rescue => exc
+            logger.error exc
+            control[:status] = 'API-ERROR'
           end
+          json_body control
         end
+      end
 
-        namespace '/status' do
-          get PATH_RX do
-            status = store.fetch_status!(path)
-            json_body(server_metadata.merge(status: status))
-          end
+      namespace '/status' do
+        get PATH_RX do
+          status = store.fetch_status!(path)
+          json_body(server_metadata.merge(status: status))
         end
+      end
 
-        namespace '/control' do
-          get PATH_RX do
-            if control = store.get_control!(path)
-              json_body(control)
-            else
-              404
-            end
-          end
-          put PATH_RX do
-            control = store.put_control!(path, from_json(request.body))
-            json_body control
-          end
-          patch PATH_RX do
-            control = store.patch_control!(path, from_json(request.body))
-            json_body control
-          end
-          delete PATH_RX do
-            if control = store.delete_control!(path)
-              json_body(control)
-            else
-              404
-            end
+      namespace '/control' do
+        get PATH_RX do
+          if control = store.get_control!(path)
+            json_body(control)
+          else
+            404
           end
         end
+        put PATH_RX do
+          control = store.put_control!(path, from_json(request.body))
+          json_body control
+        end
+        patch PATH_RX do
+          control = store.patch_control!(path, from_json(request.body))
+          json_body control
+        end
+        delete PATH_RX do
+          if control = store.delete_control!(path)
+            json_body(control)
+          else
+            404
+          end
+        end
+      end
 
       helpers do
         include Constants, ControlPath::Json
